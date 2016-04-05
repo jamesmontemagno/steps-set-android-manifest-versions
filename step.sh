e3c7d922369f50e3daee24b018d434515df1f39b
Karolis Stašaitis <stkarolis@gmail.com>
2016-04-05 15:24:45 +0200

    version_name is now an optional parameter

--- step.sh
+++ step.sh
@@ -19,18 +19,14 @@ if [ -z "${version_code}" ] ; then
   exit 1
 fi
 
-if [ -z "${version_name}" ] ; then
-  echo " [!] No version_name specified!"
-  exit 1
-fi
-
 # ---------------------
 # --- Configs:
 
 echo " (i) Provided Android Manifest path: ${manifest_file}"
 echo " (i) Verson Code: ${version_code}"
-echo " (i) Version Name: ${version_name}"
-
+if ! [ -z "${version_name}" ] ; then
+  echo " (i) Version Name: ${version_name}"
+fi
 
 VERSIONCODE=`grep versionCode ${manifest_file} | sed 's/.*versionCode="//;s/".*//'`
 VERSIONNAME=`grep versionName ${manifest_file} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
@@ -59,9 +55,10 @@ set -v
 
 sed -i.bak "s/android:versionCode="\"${VERSIONCODE}\""/android:versionCode="\"${version_code}\""/" ${manifest_file}
 
-# ---- Set Build Version Code:
-
-sed -i.bak "s/android:versionName="\"${VERSIONNAME}\""/android:versionName="\"${version_name}\""/" ${manifest_file}
+# ---- Set Build Version Code if it was specified:
+if ! [ -z "${version_name}" ] ; then
+  sed -i.bak "s/android:versionName="\"${VERSIONNAME}\""/android:versionName="\"${version_name}\""/" ${manifest_file}
+fi
 
 
 # ---- Remove backup:
